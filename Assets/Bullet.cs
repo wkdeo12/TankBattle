da190a423a4b26cd1ea1e2c8cc0189d35440294c
wkdeo12 <43337006+wkdeo12@users.noreply.github.com>
2019-03-01 18:09:36 +0900

    탱크프리팹 및 총알 프리팹  맵 생성

---
+++ Assets/Bullet.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    private Rigidbody rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Start()
+    {
+        rb.AddForce(Vector3.forward * 300f);
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+    }
+}
---
+++ Assets/BulletManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletManager : MonoBehaviour
+{
+    private Rigidbody rb;
+
+    private void Awake()
+    {
+        rb.GetComponent<Rigidbody>();
+    }
+
+    private void Start()
+    {
+        rb.AddForce(Vector3.forward * 200);
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+    }
+}

5daf8ed194e7ac8072594e36d46be32d6073aa3e
hyun19899 <hyun19899@naver.com>
2019-03-03 18:31:15 +0900

    Move 스크립트
    각도 스피드 변수 프로퍼티
    전진 기능 구현
    각도회전 기능 구현중

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -1,18 +1,54 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Move : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private float angle;
+
+    public float Angle
     {
-        
+        get => angle;
+        set => angle = Mathf.Abs(value);
     }
 
-    // Update is called once per frame
-    void Update()
+    private float speed;
+
+    public float Speed
     {
-        
+        get => speed;
+        set => speed = Mathf.Abs(value);
+    }
+
+    /// <summary>
+    /// 전진
+    /// </summary>
+    public void Advance(float speed)
+    {
+        transform.Translate(Vector3.forward * Mathf.Abs(speed) * Time.deltaTime);
+    }
+
+    /// <summary>
+    /// 각도 회전
+    /// </summary>
+    public void AngleRotRight(float angle)
+    {
+        StartCoroutine(Rotation(angle));
+    }
+
+    public void AngleRotLeft(float angle)
+    {
+        angle = -angle;
+        StartCoroutine(Rotation(angle));
+    }
+
+    public IEnumerator Rotation(float angle)
+    {
+        float age;
+        for (; ; )
+        {
+            age = Mathf.Lerp(0, angle, Time.deltaTime);
+
+            yield return null;
+        }
     }
 }

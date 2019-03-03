9a53830d639dff0ffcbbf2555147398c2949553a
wkdeo12 <43337006+wkdeo12@users.noreply.github.com>
2019-03-03 18:29:34 +0900

    펑션데이터리스트 만듬

---
+++ Assets/MyDD/FunctionDataBase.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public enum MoveFunctionList { MoveFoward, MoveBackward, RotateRight, RotateLeft }
+
+public class FunctionDataBase : MonoBehaviour
+{
+    private List<Action> moveFuncDataBase = new List<Action>();
+
+    private void Start()
+    {
+        ListUp();
+    }
+
+    public void ListUp()
+    {
+    }
+}
